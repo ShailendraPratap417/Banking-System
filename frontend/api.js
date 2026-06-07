3fc378832f92fd7bc5c83722bb6757bf1f2859f7
ShailendraPratap417 <shailendrap809@gmail.com>
2026-06-07 10:37:05 +0530

    fix api connection for production

--- frontend/api.js
+++ frontend/api.js
@@ -4,4 +4,10 @@ const api = axios.create({
   baseURL: "https://banking-system-production-c33a.up.railway.app"
 });
 
+export default api;import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://banking-system-production-c33a.up.railway.app/api"
+});
+
 export default api;
